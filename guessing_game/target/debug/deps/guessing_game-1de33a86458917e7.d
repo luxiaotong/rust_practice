6b91b38b5754da6942fff344d7f442817c38ada0
Shannon Lu <luxiaotong713@sina.com>
2022-11-09 15:20:05 +0800

    move guessing game to tutorial

--- guessing_game/target/debug/deps/guessing_game-1de33a86458917e7.d
+++
@@ -1,5 +0,0 @@
-/Users/shannon/code/rust_practice/guessing_game/target/debug/deps/guessing_game-1de33a86458917e7: src/main.rs
-
-/Users/shannon/code/rust_practice/guessing_game/target/debug/deps/guessing_game-1de33a86458917e7.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/shannon/code/rust_practice/guessing_game/target/debug/guessing_game: /Users/shannon/code/rust_practice/guessing_game/src/main.rs
