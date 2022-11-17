71f8c6217bd5d267ea68b494da09f00ff9341369
Shannon Lu <luxiaotong713@sina.com>
2022-11-17 16:02:41 +0800

    tutorial - restaurant

--- restaurant/target/debug/librestaurant.d
+++
@@ -1,1 +0,0 @@
-/Users/shannon/code/rust_practice/restaurant/target/debug/librestaurant.rlib: /Users/shannon/code/rust_practice/restaurant/src/lib.rs
---
+++ tutorial/restaurant/src/lib.rs
@@ -0,0 +1,44 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Self {
+            Self {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+        pub fn fruit(&self) {
+            println!("seasonal_fruit: {}", self.seasonal_fruit);
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    meal.fruit();
+}
